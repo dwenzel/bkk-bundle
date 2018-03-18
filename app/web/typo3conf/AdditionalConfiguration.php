982d4aaaeab52a8e957aed8bdd65ad738c4a65cf
Dirk Wenzel <der-wenzel@gmx.de>
2018-03-18 22:43:32 +0100

    [TASK] load credentials and configuration from env file (using helhum/configloader and primed LocalConfiguration and AdditionalConfiguration files)

---
+++ app/web/typo3conf/AdditionalConfiguration.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+$confDir = __DIR__ . '/../../../';
+$configReaderFactory = new \Helhum\ConfigLoader\ConfigurationReaderFactory($confDir);
+$configLoader = new \Helhum\ConfigLoader\ConfigurationLoader(
+    [
+        $configReaderFactory->createRootReader($confDir . 'env.yml'),
+    ]
+);
+$config = $configLoader->load();
+$GLOBALS['TYPO3_CONF_VARS'] = array_replace_recursive(
+    $GLOBALS['TYPO3_CONF_VARS'],
+    $config
+);
---
+++ app/web/typo3conf/LocalConfiguration.php
@@ -0,0 +1,32 @@
+<?php
+return [
+    'EXT' => [
+        'extConf' => [
+            'mediace' => 'a:0:{}',
+            'rsaauth' => 'a:1:{s:18:"temporaryDirectory";s:0:"";}',
+            'saltedpasswords' => 'a:2:{s:3:"BE.";a:4:{s:21:"saltedPWHashingMethod";s:41:"TYPO3\\CMS\\Saltedpasswords\\Salt\\Pbkdf2Salt";s:11:"forceSalted";i:0;s:15:"onlyAuthService";i:0;s:12:"updatePasswd";i:1;}s:3:"FE.";a:5:{s:7:"enabled";i:1;s:21:"saltedPWHashingMethod";s:41:"TYPO3\\CMS\\Saltedpasswords\\Salt\\Pbkdf2Salt";s:11:"forceSalted";i:0;s:15:"onlyAuthService";i:0;s:12:"updatePasswd";i:1;}}',
+        ],
+    ],
+    'FE' => [
+        'debug' => true,
+        'loginSecurityLevel' => 'rsa',
+    ],
+    'GFX' => [
+        'jpg_quality' => '80',
+        'processor' => 'ImageMagick',
+        'processor_allowTemporaryMasksAsPng' => '',
+        'processor_colorspace' => 'RGB',
+        'processor_effects' => '0',
+        'processor_enabled' => '1',
+        'processor_path' => '/hp/bs/ab/ou/www/im/bin/',
+        'processor_path_lzw' => '/hp/bs/ab/ou/www/im/bin/',
+    ],
+    'MAIL' => [
+        'transport' => 'sendmail',
+        'transport_sendmail_command' => '/usr/sbin/sendmail -t -i ',
+        'transport_smtp_encrypt' => '',
+        'transport_smtp_password' => '',
+        'transport_smtp_server' => '',
+        'transport_smtp_username' => '',
+    ]
+];
